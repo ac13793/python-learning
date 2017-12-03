8a969ea41affe71d7db7576ff2341a38cf915f8e
Ankit Chaurasia <ac13793@gmail.com>
2017-12-03 17:53:10 +0530

    Changed color of map marker

--- Application 2 Creating Webmaps with Python and Folium/foliumMap.py
+++ Application 2 Creating Webmaps with Python and Folium/foliumMap.py
@@ -6,9 +6,22 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location = [30.58, -99.09], zoom_start = 6, tiles = "Mapbox Bright")
 fg = folium.FeatureGroup(name = "My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) +" m", icon=folium.Icon(color='green')))
+    # fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) +" m", icon=folium.Icon(color='green')))
+    # TO show circle markers
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) +" m", fill_color = color_producer(el), color = 'grey', fill_opacity=0.7))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ "m",
+    fill_color = color_producer(el), color='grey', fill_opacity=0.7))
+
 map.add_child(fg)
 map.save("foliumMap.html")

14b39cf6bae3412b278a2a4b0948bd5374f855df
Ankit Chaurasia <ac13793@gmail.com>
2017-12-09 16:12:44 +0530

    String

---
+++ strings.py
@@ -0,0 +1,8 @@
+fruit = 'banana'
+print("fruit[0:3]:", fruit[0:3]) # fruit[n, m] including n but excluding m
+
+print("fruit[:3]:", fruit[:3])
+
+print("fruit[2:]:", fruit[2:])
+
+print("fruit[:]", fruit[:])
